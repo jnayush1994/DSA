2b1653139b4accda57a376b58238203d6ccd86f3
Ayush Jain <jnayush15101994@gmail.com>
2026-01-15 11:30:57 +0530

    Time: 3 ms (69.51%) | Memory: 56 MB (61.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -10,7 +10,7 @@ var isValid = function (s) {
         "[": "]"
     }
     for (let i = 0; i < s.length; i++) {
-        if (map.hasOwnProperty(s[i])) stack.push(s[i]);
+        if (map[s[i]]) stack.push(s[i]);
         else {
             if (map[stack[stack.length - 1]] === s[i]) stack.pop();
             else return false;
