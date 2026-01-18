3937f0fc09ad569c29ef1fd2677940801716111e
Ayush Jain <jnayush15101994@gmail.com>
2026-01-18 11:09:35 +0530

    Time: 19 ms (53.21%) | Memory: 77.1 MB (73.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -7,18 +7,18 @@ var dailyTemperatures = function (temperatures) {
     let stack = [];
 
     output.push(0);
-    stack.push([temperatures[temperatures.length - 1], temperatures.length - 1]);
+    stack.push(temperatures.length - 1);
 
     for (let i = temperatures.length - 2; i >= 0; i--) {
         while (stack.length) {
-            if (temperatures[i] < stack[stack.length - 1][0]) {
-                output.push(stack[stack.length - 1][1] - i);
+            if (temperatures[i] < temperatures[stack[stack.length - 1]]) {
+                output.push(stack[stack.length - 1] - i);
                 break;
             }
             else stack.pop();
         }
         if (stack.length === 0) output.push(0);
-        stack.push([temperatures[i], i]);
+        stack.push(i);
     }
 
     return output.reverse();
