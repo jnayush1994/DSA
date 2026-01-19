bb7d87c980c8afd16a74464227a3ab1cf3603de7
Ayush Jain <jnayush15101994@gmail.com>
2026-01-19 10:37:27 +0530

    Time: 1 ms (71.64%) | Memory: 57.2 MB (15.98%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -2,15 +2,29 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    let prev = 0;
-    let curr = 1;
+var mySqrt = function (x) {
+    // let prev = 0;
+    // let curr = 1;
 
-    while(curr*curr < x){
-        ++curr;
-        ++prev;
+    // while(curr*curr < x){
+    //     ++curr;
+    //     ++prev;
+    // }
+
+    // if(curr*curr === x) return curr;
+    // else return prev;
+
+    if (x < 2) return x;
+
+    let l = 2;
+    let r = Math.floor(x / 2);
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (x === m*m) return m;
+        else if (m*m > x) r = m - 1;
+        else if (m*m < x) l = m + 1;
     }
 
-    if(curr*curr === x) return curr;
-    else return prev;
+    return r;
 };
