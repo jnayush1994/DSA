48e6bfe0190e43d01b038622aa61acecef4e8f4f
Ayush Jain <jnayush15101994@gmail.com>
2026-01-26 06:47:35 +0530

    Time: 3 ms (59.12%) | Memory: 56.3 MB (30.80%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,9 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if(nums[i] + nums[j] === target) return [i,j];
+    //     }
+    // }
+    let map = {};
+
+    for (let i = 0; i < nums.length; i++)
+        map[nums[i]] = i;
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) return [i,j];
-        }
+        let pairToFind = target - nums[i];
+        if (map[pairToFind] && map[pairToFind] !== i) return [i, map[pairToFind]];
     }
 };
