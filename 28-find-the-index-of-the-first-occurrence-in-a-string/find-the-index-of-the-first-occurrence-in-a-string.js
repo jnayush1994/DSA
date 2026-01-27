481cfba2992d6db476266d5db9a4da5886dbead3
Ayush Jain <jnayush15101994@gmail.com>
2026-01-27 07:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (36.90%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,7 +7,7 @@ var strStr = function (haystack, needle) {
     let n = haystack.length;
     let m = needle.length;
 
-    for (let i = 0; i < haystack.length; i++) {
+    for (let i = 0; i <= n - m; i++) {
         let j = 0;
         for (j = 0; j < m; j++) {
             if (haystack[i + j] !== needle[j]) break;
