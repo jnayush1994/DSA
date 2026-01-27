e681def2b0116330c73f7589782911a0978c6959
Ayush Jain <jnayush15101994@gmail.com>
2026-01-27 06:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (29.95%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -6,7 +6,7 @@
 var isSubsequence = function(s, t) {
     let i = 0;
     for(let j = 0; j < t.length; j++){
-        if(s[i] === t[j]) i++;
+        if(s[i] === t[j]) ++i;
     }
     return i === s.length;
 };
