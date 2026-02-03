e2a5c7f9371b8bcca27303d226838a49ef04d9e2
Ayush Jain <jnayush15101994@gmail.com>
2026-02-03 09:30:08 +0530

    Time: 433 ms (40.80%) | Memory: 85.8 MB (60.30%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+    let result = [];
+    let queue = [];
+
+    let i = j = 0;
+
+    while (j < nums.length) {
+        while (queue.length && nums[j] > queue[queue.length - 1]) queue.pop();
+        queue.push(nums[j]);
+
+        if (j >= k - 1) {
+            result.push(queue[0]);
+            nums[i] === queue[0] && queue.shift();
+            ++i;
+        }
+        ++j;
+    }
+
+    return result;
+};
