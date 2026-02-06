5a2ca05df06db3b97bdbd693f2c95702ea5acb16
Ayush Jain <jnayush15101994@gmail.com>
2026-02-06 08:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (85.33%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -11,13 +11,29 @@
  * @return {number[]}
  */
 var postorderTraversal = function (root) {
+    // let result = [];
+    // let traversal = function (curr) {
+    //     if (!curr) return;
+    //     traversal(curr.left);
+    //     traversal(curr.right);
+    //     result.push(curr.val);
+    // }
+    // traversal(root);
+    // return result;
     let result = [];
-    let traversal = function (curr) {
-        if (!curr) return;
-        traversal(curr.left);
-        traversal(curr.right);
-        result.push(curr.val);
+    let s1 = [];
+    let s2 = [];
+    let current;
+
+    if (root) s1.push(root);
+    else return result;
+
+    while (s1.length) {
+        current = s1.pop();
+        s2.push(current.val);
+        if (current.left) s1.push(current.left);
+        if (current.right) s1.push(current.right);
     }
-    traversal(root);
-    return result;
+
+    return s2.reverse();
 };
