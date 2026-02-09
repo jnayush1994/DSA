9ae45b075910f6643bd66f8ac97cb7e62fe3dc23
Ayush Jain <jnayush15101994@gmail.com>
2026-02-09 08:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 56.7 MB (63.73%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.js
+++ 101-symmetric-tree/symmetric-tree.js
@@ -11,10 +11,31 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-    let isMirror = function (left, right) {
-        if (!left && !right) return true;
-        if (!left || !right) return false;
-        return left.val === right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    // let isMirror = function (left, right) {
+    //     if (!left && !right) return true;
+    //     if (!left || !right) return false;
+    //     return left.val === right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    // }
+    // return isMirror(root.left, root.right);
+    if (!root.left && !root.right) return true;
+    if (!root.left || !root.right) return false;
+
+    let queue = [root.left, root.right];
+    let n1 = null;
+    let n2 = null;
+
+    while (queue.length) {
+        n1 = queue.shift();
+        n2 = queue.shift();
+
+        if (!n1 && !n2) continue;
+        if (!n1 || !n2) return false;
+        if (n1.val !== n2.val) return false;
+
+        queue.push(n1.left);
+        queue.push(n2.right);
+        queue.push(n1.right);
+        queue.push(n2.left);
     }
-    return isMirror(root.left, root.right);
+    return true;
 };
