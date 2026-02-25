92cfd6c0b4f909c26c1656efc2fafb59e7d7efac
Ayush Jain <jnayush15101994@gmail.com>
2026-02-25 11:14:34 +0530

    Time: 1 ms (85.85%) | Memory: 57.7 MB (61.79%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+    let buildBST = function (left, right) {
+        if (left > right) return null;
+
+        let mid = Math.floor((left + right) / 2);
+
+        let root = new TreeNode(nums[mid]);
+
+        root.left = buildBST(left, mid - 1);
+        root.right = buildBST(mid + 1, right);
+
+        return root;
+    }
+    return buildBST(0, nums.length - 1);
+};
