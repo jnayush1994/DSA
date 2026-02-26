b8398185a0b318c737710fac95d35f0c54003a2b
Ayush Jain <jnayush15101994@gmail.com>
2026-02-26 11:49:12 +0530

    Time: 3 ms (47.22%) | Memory: 87.2 MB (39.82%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+    if (!root) return 0;
+
+    let result = root.val >= low && root.val <= high ? root.val : 0;
+    result += rangeSumBST(root.left, low, high);
+    result += rangeSumBST(root.right, low, high);
+    return result;
+};
