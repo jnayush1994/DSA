0c4a2681d96b63817a70fee4086028616ee4f72b
Ayush Jain <jnayush15101994@gmail.com>
2026-03-13 11:27:17 +0530

    Time: 7 ms (28.62%) | Memory: 56 MB (43.23%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+    const heap = new MaxHeap(null, stones);
+
+    while (heap.size() > 1) {
+        let largest = heap.extractRoot();
+        let secondLargest = heap.extractRoot();
+
+        if (largest - secondLargest > 0)
+            heap.insert(largest - secondLargest)
+    }
+
+    return heap.root() || 0;
+};
