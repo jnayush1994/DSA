34bd869be28548cf18d52131526cb4f342f6f784
Ayush Jain <jnayush15101994@gmail.com>
2026-03-24 11:05:47 +0530

    Time: 33 ms (25.09%) | Memory: 77.7 MB (15.30%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,11 +4,9 @@
  */
 var containsDuplicate = function(nums) {
     const map = {};
-
     for(let num of nums){
         if(map[num]) return true;
         else map[num] = 1;
     }
-
     return false;
 };
