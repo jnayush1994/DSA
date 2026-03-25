66e57f5c1be417c731167fa9e328bb4d8e0aa404
Ayush Jain <jnayush15101994@gmail.com>
2026-03-25 10:49:54 +0530

    Time: 57 ms (16.75%) | Memory: 77.9 MB (59.91%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -7,13 +7,10 @@ var containsNearbyDuplicate = function (nums, k) {
     const map = {};
 
     for (let i = 0; i < nums.length; i++) {
-        if (map[nums[i]] !== undefined) {
-            if (Math.abs(map[nums[i]] - i) <= k) return true;
-            else map[nums[i]] = i;
-        } else map[nums[i]] = i;
+        if (map[nums[i]] !== undefined && (Math.abs(map[nums[i]] - i) <= k))
+            return true;
+        else map[nums[i]] = i;
     }
-
-
 
     return false;
 };
