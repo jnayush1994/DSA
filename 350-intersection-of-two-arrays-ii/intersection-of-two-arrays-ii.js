abafcd43e14e256846d79603f9ee4019ca9ebf82
Ayush Jain <jnayush15101994@gmail.com>
2026-03-27 10:44:37 +0530

    Time: 1 ms (85.78%) | Memory: 55.5 MB (57.93%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -6,16 +6,13 @@
 var intersect = function (nums1, nums2) {
     const map = {};
     const result = [];
-
     for (let num of nums1)
         map[num] = map[num] ? ++map[num] : 1;
-
     for(let num of nums2){
         if(map[num] && map[num] > 0){
             result.push(num);
             map[num] = --map[num];
         }
     }
-
     return result;    
 };
