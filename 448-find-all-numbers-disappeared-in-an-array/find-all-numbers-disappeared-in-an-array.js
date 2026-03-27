59b63121ec29caa50a6a04d0ada63f1abeea6d59
Ayush Jain <jnayush15101994@gmail.com>
2026-03-27 11:29:33 +0530

    Time: 22 ms (35.86%) | Memory: 76.9 MB (25.39%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -6,11 +6,9 @@ var findDisappearedNumbers = function (nums) {
     const set = new Set(nums);
     const n = nums.length;
     const output = [];
-
     for (let i = 1; i <= n; i++) {
         if (!set.has(i))
             output.push(i);
     }
-
     return output;
 };
