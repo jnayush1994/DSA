658a831cc91d1c700a6bc74535b994ba37c269c7
Ayush Jain <jnayush15101994@gmail.com>
2026-04-02 09:10:51 +0530

    Time: 67 ms (40.59%) | Memory: 128.4 MB (66.25%) - LeetSync

---
+++ 77-combinations/combinations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+    const result = [];
+
+    const backTrack = (path, startIdx) => {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = startIdx; i <= n; i++) {
+            path.push(i);
+            backTrack(path, i + 1);
+            path.pop();
+        }
+    }
+
+    backTrack([], 1);
+
+    return result;
+};
