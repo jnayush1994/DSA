b1b5576102ce85e6e2dc88bbf3874c52df09251c
Ayush Jain <jnayush15101994@gmail.com>
2026-04-02 10:03:34 +0530

    Time: 2 ms (64.82%) | Memory: 58.9 MB (84.31%) - LeetSync

---
+++ 46-permutations/permutations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    const result = [];
+
+    const backTrack = (path, currIdx) => {
+        if(path.length === nums.length){
+            result.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (!path.includes(nums[i])) {
+                path.push(nums[i]);
+                backTrack(path, i);
+                path.pop();
+            }
+        }
+    };
+
+    backTrack([]);
+
+    return result;
+};
