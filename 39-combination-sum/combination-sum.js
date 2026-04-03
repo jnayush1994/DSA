1d80580457015ef3f80ea4388e41b046a39dedda
Ayush Jain <jnayush15101994@gmail.com>
2026-04-03 12:49:18 +0530

    Time: 1 ms (96.73%) | Memory: 60.2 MB (39.44%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -5,22 +5,17 @@
  */
 var combinationSum = function (candidates, target) {
     const result = [];
-
     const backTrack = (remainingSum, path, startIdx) => {
         if (remainingSum === 0)
             result.push([...path]);
-
         if (remainingSum <= 0)
             return;
-
         for (let i = startIdx; i < candidates.length; i++) {
             path.push(candidates[i]);
             backTrack(remainingSum - candidates[i], path, i);
             path.pop();
         }
     };
-
     backTrack(target, [], 0);
-
     return result;
 };
