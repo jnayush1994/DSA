5972d0c5be6b6920b7947b30a23b88826885aa58
Ayush Jain <jnayush15101994@gmail.com>
2026-04-23 09:36:08 +0530

    Time: 27 ms (38.60%) | Memory: 62.3 MB (55.25%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    let count = 0;
+
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+
+    let i = 0;
+    let j = 0;
+
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
+            count++;
+            i++;
+            j++;
+        }
+        else {
+            j++;
+        }
+    }
+
+    return count;
+};
