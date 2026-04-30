3ba26fff677c7231d7dec4c93b4cb7f358c89255
Ayush Jain <jnayush15101994@gmail.com>
2026-04-30 11:40:06 +0530

    Time: 11 ms (19.00%) | Memory: 60.5 MB (12.26%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -6,26 +6,21 @@ var candy = function (ratings) {
     let n = ratings.length;
 
     if (n === 1) return n;
-
     let ltr = Array(n).fill(1);
     let rtl = Array(n).fill(1);
     let result = 0;
-
     for (let i = 1; i < n; i++) {
         if (ratings[i] > ratings[i - 1]) {
             ltr[i] = ltr[i - 1] + 1;
         }
     }
-
     for (let i = n - 2; i >= 0; i--) {
         if (ratings[i] > ratings[i + 1]) {
             rtl[i] = rtl[i + 1] + 1;
         }
     }
-
     for(let i = 0; i < n; i++){
         result += Math.max(ltr[i], rtl[i]);
     }
-
     return result;
 };
