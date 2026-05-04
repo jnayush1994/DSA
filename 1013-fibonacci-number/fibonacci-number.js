82bc16f25fdf7e8e7c39470603927cda80128c8a
Ayush Jain <jnayush15101994@gmail.com>
2026-05-04 10:33:01 +0530

    Time: 20 ms (100.00%) | Memory: 54.9 MB (12.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,11 +3,22 @@
  * @return {number}
  */
 
-const store = {};
+// const store = {};
+
+// var fib = function (n) {
+//     if (n <= 1) return n;
+//     if (!store[n])
+//         store[n] = fib(n - 1) + fib(n - 2);
+//     return store[n];
+// };
+
 
 var fib = function (n) {
-    if (n <= 1) return n;
-    if (!store[n])
-        store[n] = fib(n - 1) + fib(n - 2);
-    return store[n];
+    const dp = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
 };
