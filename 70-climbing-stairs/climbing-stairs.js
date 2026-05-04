b04ad0ac8bb434dfca0e4bb4948898425b15e37e
Ayush Jain <jnayush15101994@gmail.com>
2026-05-04 11:22:25 +0530

    Time: 1 ms (5.53%) | Memory: 53.5 MB (55.39%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -3,9 +3,15 @@
  * @return {number}
  */
 
-const dp = {};
+// const dp = {};
+// var climbStairs = function (n) {
+//     if (n <= 1) return 1;
+//     if (!dp[n]) dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+//     return dp[n];
+// };
+
 var climbStairs = function (n) {
-    if (n <= 1) return 1;
-    if (!dp[n]) dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
-    return dp[n];
+    const dp = [0, 1, 2];
+    for (let i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+    return dp[n]
 };
