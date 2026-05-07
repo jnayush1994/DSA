1257b3d9818e8736dfd47157acfa44692ee7af4c
Ayush Jain <jnayush15101994@gmail.com>
2026-05-07 10:00:15 +0530

    Time: 3 ms (35.52%) | Memory: 57 MB (14.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -8,8 +8,13 @@ var twoSum = function (numbers, target) {
     let j = numbers.length - 1;
 
     while (i < j) {
-        if (numbers[i] + numbers[j] === target) return [i + 1, j + 1];
-        else if((numbers[i] + numbers[j]) > target) j--;
-        else i++;
+        if (numbers[i] + numbers[j] < target)
+            i++;
+
+        if (numbers[i] + numbers[j] > target)
+            j--;
+
+        if (numbers[i] + numbers[j] === target)
+            return [i + 1, j + 1];
     }
 };
