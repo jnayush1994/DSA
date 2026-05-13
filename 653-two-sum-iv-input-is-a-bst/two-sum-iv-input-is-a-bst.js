d8609fab15833982956f020bbc371d1c1968ab85
Ayush Jain <jnayush15101994@gmail.com>
2026-05-13 09:40:59 +0530

    Time: 4 ms (35.11%) | Memory: 63.4 MB (92.48%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function (root, k) {
+    const store = [];
+
+    const traversal = (curr) => {
+        if (!curr) return;
+        traversal(curr.left);
+        store.push(curr.val);
+        traversal(curr.right);
+    }
+
+    traversal(root);
+
+    let i = 0;
+    let j = store.length - 1;
+
+    while (i < j) {
+        let sum = store[i] + store[j];
+        if (sum === k)
+            return true;
+
+        if (sum < k)
+            i++;
+
+        if (sum > k)
+            j--;
+    }
+
+    return false;
+};
