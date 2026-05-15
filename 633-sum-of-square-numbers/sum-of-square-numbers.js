4ed4c36767f92e7a02a797a9b0ef0866ec7502be
Ayush Jain <jnayush15101994@gmail.com>
2026-05-15 19:13:04 +0530

    Time: 5 ms (68.90%) | Memory: 55.5 MB (53.05%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.js
+++ 633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -5,20 +5,14 @@
 var judgeSquareSum = function (c) {
     let i = 0;
     let j = Math.floor(Math.sqrt(c));
-
     if (c === 0 || c === 1) return true
-
     while (i < j) {
         let sum = i ** 2 + j ** 2;
         let sumi = i ** 2 + i ** 2;
         let sumj = j ** 2 + j ** 2;
-
         if (sum === c || sumi === c || sumj === c) return true;
-
         if (sum > c) j--;
-
         if (sum < c) i++;
     }
-
     return false;
 };
